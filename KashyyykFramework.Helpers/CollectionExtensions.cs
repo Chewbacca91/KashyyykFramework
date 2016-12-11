90f0b92c23732488a1ac5594d9364407b1712412
Chewbacca <Chewbacca@CHEWBACCA-PC>
2016-12-11 13:13:08 +0100

    Ajout du projets 'Helpers'. CollectionExtensions avec une méthode splitant les IEnumerable en liste de IEnumerable (pour MVC et Bootstrap)

---
+++ KashyyykFramework.Helpers/CollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KashyyykFramework.Helpers
+{
+    /// <summary>
+    /// Méthodes d'extensions ajoutant des fonctionnalités aux collections
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array">Collection de départ</param>
+        /// <param name="nbDim">Nombre de dimensions en sortie</param>
+        /// <returns>
+        /// Listes d'énumérables de la taille définie
+        /// </returns>
+        public static IEnumerable<IEnumerable<T>> SplitMultiDim<T>(this IEnumerable<T> array, int size)
+        {
+            for (int i = 0; i < (float)array.Count() / size; i++)
+            {
+                yield return array.Skip(i * size).Take(size);
+            }
+        }
+    }
+}
