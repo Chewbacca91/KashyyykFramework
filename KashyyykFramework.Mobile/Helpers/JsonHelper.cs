3295dc8735195418bab5823327284d1f57ae9590
Chewbacca <Chewbacca@CHEWBACCA-PC>
2017-01-14 19:29:19 +0100

    Ajout du projet Portable Class Library pour les plate-formes mobiles

---
+++ KashyyykFramework.Mobile/Helpers/JsonHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace KashyyykFramework.Mobile.Helpers
+{
+    public static class JsonHelper
+    {
+        /// <summary>
+        /// Sérialisation d'un objet en JSON
+        /// </summary>
+        /// <typeparam name="T">Type de l'objet</typeparam>
+        /// <param name="obj">objet à sérialiser</param>
+        /// <returns>
+        /// Chaine représentant l'objet en JSON
+        /// </returns>
+        public static string SerializeJson<T>(T obj)
+        {
+            DataContractJsonSerializer jsonSerializer = CreateDataContractJsonSerializer(obj);
+            byte[] streamArray = null;
+            using (var memoryStream = new MemoryStream())
+            {
+                jsonSerializer.WriteObject(memoryStream, obj);
+                streamArray = memoryStream.ToArray();
+            }
+            string json = Encoding.UTF8.GetString(streamArray, 0, streamArray.Length);
+            return json;
+        }
+
+        /// <summary>
+        /// Désérialisation d'un objet JSON
+        /// </summary>
+        /// <typeparam name="T">Type de l'objet</typeparam>
+        /// <param name="obj">objet dans lequel sérialiser</param>
+        /// <param name="json">chaine json contenant les données de l'objet</param>
+        public static T DeserializeJson<T>(string json)
+        {
+            T obj = default(T);
+            if (string.IsNullOrWhiteSpace(json)) return obj;
+            DataContractJsonSerializer jsonSerializer = CreateDataContractJsonSerializer(obj);
+            using (Stream s = json.ToStream())
+            {
+                obj = (T)jsonSerializer.ReadObject(s);
+            }
+            return obj;
+        }
+
+        /// <summary>
+        /// Méthode de création d'un jSon serializer pour le type d'objet spécifié
+        /// Settings peut etre spécifié
+        /// </summary>
+        /// <typeparam name="T">Type d'objet pour lequel instancier le sérializer</typeparam>
+        /// <param name="type">Type d'objet pour lequel instancier le sérializer</param>
+        /// <param name="settings">Settings (optionels)</param>
+        /// <returns>
+        /// DataContractJsonSerializer instancié
+        /// </returns>
+        private static DataContractJsonSerializer CreateDataContractJsonSerializer<T>(T type, DataContractJsonSerializerSettings settings = null)
+        {
+            if (settings != null) return new DataContractJsonSerializer(typeof(T), settings);
+            return new DataContractJsonSerializer(typeof(T));
+        }
+
+    }
+}
---
+++ KashyyykFramework.Utiliy/Http/WebApiProvider.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using KashyyykFramework.Helpers;
+
+namespace KashyyykFramework.Utiliy.Http
+{
+    public class WebApiProvider<T> : IDisposable
+    {
+        HttpClient _httpClient = null;
+        string _baseUrl = null;
+        string _servicePath = null;
+
+        public WebApiProvider(string baseUrl, string servicePath)
+        {
+            this._baseUrl = baseUrl;
+            this._servicePath = servicePath;
+            this._httpClient = CreateHttpClient(this._baseUrl);
+        }
+
+        public IList<T> Get()
+        {
+            string responseContent = this.SendRequestSynchrone(this._httpClient.GetAsync, this._servicePath);
+
+            if (responseContent != null)
+            {
+                IList<T> lst = JsonHelper.DeserializeJson<IList<T>>(responseContent);
+                return lst;
+            }
+
+            return new List<T>();
+        }
+
+        public T Get(object value)
+        {
+            string uri = $"{this._servicePath}/{value.ToString()}";
+            string responseContent = this.SendRequestSynchrone(this._httpClient.GetAsync, uri);
+
+            if (responseContent != null)
+            {
+                T item = JsonHelper.DeserializeJson<T>(responseContent);
+                return item;
+            }
+
+            return default(T);
+        }
+
+        public IList<T> Get(string addPath, IDictionary<string, object> urlParameters = null)
+        {
+            string uri = GetFullUri(addPath, urlParameters);
+
+            string responseContent = this.SendRequestSynchrone(this._httpClient.GetAsync, uri);
+            if (responseContent != null)
+            {
+                IList<T> lst = JsonHelper.DeserializeJson<IList<T>>(responseContent);
+                return lst;
+            }
+
+            return new List<T>();
+        }
+
+        public TValue Get<TValue>(string addPath, IDictionary<string, object> urlParameters = null)
+        {
+            string uri = GetFullUri(addPath, urlParameters);
+
+            string responseContent = this.SendRequestSynchrone(this._httpClient.GetAsync, uri);
+            if (responseContent != null)
+            {
+                TValue val = JsonHelper.DeserializeJson<TValue>(responseContent);
+                return val;
+            }
+
+            return default(TValue);
+        }
+
+        public T Post(T item)
+        {
+            HttpContent content = new StringContent(JsonHelper.SerializeJson(item), Encoding.UTF8, "application/json");
+            string responseContent = this.SendRequestSynchrone(this._httpClient.PostAsync, this._servicePath, content);
+            if (responseContent != null)
+            {
+                T newItem = JsonHelper.DeserializeJson<T>(responseContent);
+                return newItem;
+            }
+
+            throw new Exception("Erreur de création de la ressource !");
+        }
+
+        public void Put(object idValue, T item)
+        {
+            string uri = $"{this._servicePath}/{idValue.ToString()}";
+            HttpContent content = new StringContent(JsonHelper.SerializeJson(item), Encoding.UTF8, "application/json");
+            string responseContent = this.SendRequestSynchrone(this._httpClient.PutAsync, uri, content);
+            if (responseContent == null) throw new Exception("Erreur lors de la mise à jour de la ressource !");
+        }
+
+        public void Dispose()
+        {
+            this._httpClient?.Dispose();
+        }
+
+        #region Méthodes privées
+
+        private HttpClient CreateHttpClient(string baseUrl)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        private string GetFullUri(string addPath, IDictionary<string, object> parameters)
+        {
+            string urlQueryString = null;
+            if (parameters != null && parameters.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var kvp in parameters) { sb.Append($"&{kvp.Key}={kvp.Value.ToString()}"); }
+                urlQueryString = sb.ToString().Remove(0, 1);
+            }
+            string uri = (string.IsNullOrWhiteSpace(urlQueryString) ? $"{this._servicePath}/{addPath}" : $"{this._servicePath}/{addPath}?{urlQueryString}");
+
+            return uri;
+        }
+
+        private string SendRequestSynchrone(Func<string, Task<HttpResponseMessage>> httpClientFunc, string uri)
+        {
+            Task<string> responseContent = Task.Run(async () =>
+            {
+                HttpResponseMessage response = await httpClientFunc(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseStr = await response.Content.ReadAsStringAsync();
+                    return responseStr;
+                }
+                return null;
+            });
+
+            return responseContent?.Result;
+        }
+
+        private string SendRequestSynchrone(Func<string, HttpContent, Task<HttpResponseMessage>> httpClientFunc, string uri, HttpContent content)
+        {
+            Task<string> responseContent = Task.Run(async () =>
+            {
+                HttpResponseMessage response = await httpClientFunc(uri, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseStr = await response.Content.ReadAsStringAsync();
+                    return responseStr;
+                }
+                return null;
+            });
+
+            return responseContent?.Result;
+        }
+
+        #endregion
+    }
+}
